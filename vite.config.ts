0a68ef812bc9f29f0c907f392f05a269cebecbc5
kango furusu <furusu0715@gmail.com>
2024-11-07 23:14:54 +0900

    chrome拡張テスト

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { crx, defineManifest, } from '@crxjs/vite-plugin';
+
+const manifest = defineManifest({
+  manifest_version: 3,
+  name: "Counter",
+  version: "1.0.0",
+  permissions: [],
+  action: {
+    default_popup: "index.html",
+  },
+});
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte(), crx({ manifest})],
 })
